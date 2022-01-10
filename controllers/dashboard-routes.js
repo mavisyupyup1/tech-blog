268e4edf77f1cd6509cb2681df26dc4a392948c1
mavisyupyup1 <mavisyupyup1@github.com>
2022-01-10 09:39:39 -0500

    updated models, change all url to contents

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -11,7 +11,7 @@ router.get('/', withAuth,(req,res)=>{
         },
         attributes:[
             'id',
-            'post_url',
+            'post_content',
             'title',
             'created_at',
             [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id=vote.post_id)'),'vote_count']
